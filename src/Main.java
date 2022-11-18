caefe0263f9062b4852c2964a9fbe3ff40f2359b
EugeneH <eugeneheynike@gmail.com>
2022-11-18 14:23:35 +0200

    just adding Main in case

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,42 @@
+import functionality.JsonEditor;
+import functionality.json.jsonFunction;
 import functionality.text.StringFunctions;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+//parse option to show only valid options *
+//if value is string ask to edit
+//edit value
 
 public class Main {
+
+    static List<String> validOption = List.of("MainToolbar", "Columns", "Rows", "Title", "Widgets", "Tooltip", "Icon");
     public static void main(String[] args) {
-        String test = "THis iS not in tItle Case";
-        String test2 = "wHat";
+        JSONParser jsonParser = new JSONParser();
+        try (var reader = new FileReader("C:\\RND800\\BASE\\SAMPLES\\UX_APP_WIPPGILZ.JSON");
+             var scanner = new Scanner(System.in)) {
+            Object obj = jsonParser.parse(reader);
+            JSONObject jsonFile = (JSONObject) obj;
+//            List<String> options;
+            jsonFunction.searchLoop(obj, scanner, validOption);
 
-        String title = StringFunctions.toTitle(test);
-        System.out.println(title);
-        System.out.println("tHis".toLowerCase());
-        System.out.println(StringFunctions.toSen(test));
 
-    }
+
+            } catch (IOException | ParseException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        }
+
+
 
 }
